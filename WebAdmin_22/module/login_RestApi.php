ba11347b7da1d6813c71a0933b50ac932e814c37
RahmanIT <hidayatul.rahman0097@gmail.com>
2024-01-12 00:08:02 +0800

    membuat uato delete files SESION json login

--- WebAdmin_22/module/login_RestApi.php
+++ WebAdmin_22/module/login_RestApi.php
@@ -3,7 +3,6 @@ $dt = date('Y_m_d_h');
 $key =md5($P[0]['KD_USER'].'gsAdmin'.$dt.'kalsel');
 $taget = $conf["DataDir"].'sesion/'.$key.'.json';
 file_put_contents($taget, $_SESSION['P']);
-echo $key;
 if ($segmen3 == $key){  
 	if(isset($P)){
 		$lok = $conf["SrvGeo"].'OneMap/?token='.$key;
--- WebAdmin_22/module/login_celarfilesSesi.php
+++ WebAdmin_22/module/login_celarfilesSesi.php
@@ -1,6 +1,5 @@
 <?php 
-$file1 = $conf["DataDir"]."sesion/".$segmen3.'.json';
-echo $file1;	
+$file1 = $conf["DataDir"]."sesion/".$segmen3.'.json';	
 	if(file_exists($file1)){
 		unlink($file1);
   }else{
